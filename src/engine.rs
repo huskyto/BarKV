ffa32faced376b0ea35634a546ab63b780b9ba05
Huskyto Gonar <huskygonar@gmail.com>
2026-04-11 18:20:49 -0400

    Update engine signatures

--- src/engine.rs
+++ src/engine.rs
@@ -1,7 +1,9 @@
 
+use thiserror::Error;
+
+use crate::model::KVPair;
+use crate::model::BagKey;
 use crate::model::EntryKey;
-use crate::model::KeyValue;
-use crate::model::EngineError;
 use crate::model::StoreArchive;
 
 
@@ -12,53 +14,53 @@ pub struct BarKVEngine {
 impl BarKVEngine {
             // VALUES //
 
-    pub fn get(&self, bag: &EntryKey, key: &EntryKey) -> &[u8] {
+    pub fn get(&self, bag: &BagKey, key: &EntryKey) -> Result<Vec<u8>, EngineError> {
         todo!()     // TODO
     }
 
-    pub fn set(&mut self, bag: &EntryKey, key: &EntryKey, value: &[u8]) {
+    pub fn set(&mut self, bag: &BagKey, key: &EntryKey, value: &[u8]) -> Result<(), EngineError> {
         todo!()     // TODO
     }
 
-    pub fn delete(&mut self, bag: &EntryKey, key: &EntryKey) {
+    pub fn delete(&mut self, bag: &BagKey, key: &EntryKey) -> Result<(), EngineError> {
         todo!()     // TODO
     }
 
-    pub fn exists(&self, bag: &EntryKey, key: &EntryKey) {
+    pub fn exists(&self, bag: &BagKey, key: &EntryKey) -> bool {
         todo!()     // TODO
     }
 
-    pub fn list_keys(&self, bag: &EntryKey) -> &Vec<EntryKey> {
+    pub fn list_keys(&self, bag: &BagKey) -> Result<Vec<EntryKey>, EngineError> {
         todo!()     // TODO
     }
 
-    pub fn list_entries(&self, bag: &EntryKey) -> &Vec<KeyValue> {
+    pub fn list_entries(&self, bag: &BagKey) -> Result<Vec<KVPair>, EngineError> {
         todo!()     // TODO
     }
 
 
             // BAGS //
 
-    pub fn create_bag(&mut self, bag: &EntryKey) {
+    pub fn create_bag(&mut self, bag: &BagKey) -> Result<(), EngineError> {
         todo!()     // TODO
     }
 
-    pub fn drop_bag(&mut self, bag: &EntryKey) {
+    pub fn drop_bag(&mut self, bag: &BagKey) -> Result<(), EngineError> {
         todo!()     // TODO
     }
 
-    pub fn list_bags(&self) -> &Vec<EntryKey> {
+    pub fn list_bags(&self) -> Vec<EntryKey> {
         todo!()     // TODO
     }
 
-    pub fn count_bag(&self) -> usize {
+    pub fn count_bag(&self) -> Result<usize, EngineError> {
         todo!()     // TODO
     }
 
 
             // LIFECYCLE //
 
-    pub fn open(path: &str) -> BarKVEngine {
+    pub fn open(path: &str) -> Result<BarKVEngine, EngineError>{
         todo!()     // TODO
     }
 
@@ -77,45 +79,45 @@ impl BarKVEngine {
 
             // ATOMIC // TODO extension.
     
-    pub fn get_or_set(bag: &EntryKey, key: &EntryKey, value: &[u8]) -> Result<Vec<u8>, EngineError> {
+    pub fn get_or_set(bag: &BagKey, key: &EntryKey, value: &[u8]) -> Result<Vec<u8>, EngineError> {
         todo!()     // TODO
     }
 
-    pub fn update_if_different(bag: &EntryKey, key: &EntryKey, value: &[u8]) -> Result<(), EngineError> {
+    pub fn update_if_different(bag: &BagKey, key: &EntryKey, value: &[u8]) -> Result<(), EngineError> {
         todo!()     // TODO Maybe?
     }
 
-    pub fn get_and_delete(bag: &EntryKey, key: &EntryKey) -> Result<Vec<u8>, EngineError> {
+    pub fn get_and_delete(bag: &BagKey, key: &EntryKey) -> Result<Vec<u8>, EngineError> {
         todo!()     // TODO
     }
 
 
             // BATCH OPS // TODO Extension.
 
-    pub fn get_many(bag: &EntryKey, keys: &Vec<&EntryKey>) -> Result<Vec<KeyValue>, EngineError> {
+    pub fn get_many(bag: &BagKey, keys: &[&EntryKey]) -> Result<Vec<KVPair>, EngineError> {
         todo!()     // TODO
     }
 
-    pub fn set_many(bag: &EntryKey, pairs: &Vec<KeyValue>) -> Result<(), EngineError> {
+    pub fn set_many(bag: &BagKey, pairs: &[KVPair]) -> Result<(), EngineError> {
         todo!()     // TODO
     }
 
-    pub fn delete_many(bag: &EntryKey, keys: &Vec<&EntryKey>) -> Result<(), EngineError> {
+    pub fn delete_many(bag: &BagKey, keys: &[&EntryKey]) -> Result<(), EngineError> {
         todo!()     // TODO
     }
 
 
             // TTL // TODO Extension. Maybe.
     
-    pub fn set_with_expiry(bag: &EntryKey, key: &EntryKey, value: &[u8], ttl: u128) {
+    pub fn set_with_expiry(bag: &BagKey, key: &EntryKey, value: &[u8], ttl: u128) -> Result<(), EngineError>{
         todo!()     // TODO
     }
 
-    pub fn ttl(bag: &EntryKey, key: &EntryKey) -> Result<u128, EngineError> {
+    pub fn ttl(bag: &BagKey, key: &EntryKey) -> Result<u128, EngineError> {
         todo!()     // TODO
     }
 
-    pub fn persist(bag: &EntryKey, key: &EntryKey) {
+    pub fn persist(bag: &BagKey, key: &EntryKey) -> Result<(), EngineError> {
         todo!()     // TODO
     }
 
@@ -133,9 +135,27 @@ impl BarKVEngine {
 
             // INTERNAL //
     
-    pub fn seal(bag: &EntryKey) -> Result<String, EngineError> {
+    pub fn seal(bag: &BagKey) -> Result<String, EngineError> {
         todo!()
     }
 
 }
 
+
+#[derive(Debug, Error)]
+pub enum EngineError {
+    #[error("Wrapped IO read error: {0}")]
+    IOReadError(std::io::Error),
+    #[error("Wrapped IO write error: {0}")]
+    IOWriteError(std::io::Error),
+    #[error("EntryKey does not exist in bag: {0}")]
+    NoSuchEntryKeyError(EntryKey),
+    #[error("BagKey does not exist in store: {0}")]
+    NoSuchBagKeyError(EntryKey),
+    #[error("Entry was queried, but is deleted: {0}")]
+    DeletedEntryError(EntryKey),
+    #[error("Entry was queried, but is expired: {0}")]
+    ExpiredEntryError(EntryKey),
+    // #[error("Wrapped ParseError: {0}")]
+    // ParseError(#[from] ParseError)
+}
